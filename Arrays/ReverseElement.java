81168b9bbebdf2ad510c1bea4422f09889ff181b
ShivamDeshmukh <sadeshmukh2001@gmail.com>
2024-06-06 23:21:57 +0530

    array

--- Arrays/ReverseElement.java
+++ Arrays/ReverseElement.java
@@ -44,5 +44,8 @@ public class ReverseElement {
         return ans;
 
     }
+
+
+    
 }
 
---
+++ Arrays/ReverseElementWithSameArray.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class ReverseElementWithSameArray {
+
+    static void swapArray(int [] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+
+        for (int k = 0; k < arr.length; k++) {
+            System.out.print(arr[k]+" ");
+        }
+    }
+
+
+    static void reverseElement(int[] arr){
+        int i=0, j=arr.length-1;
+        while (i < j) {
+            swapArray(arr, i, j);
+            i++;
+            j--;
+        }
+
+        
+    }
+
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size of Array :");
+        int n= sc.nextInt();
+        int[] arr =new int[n];
+
+
+        System.out.println("Enter "+n+" Elements :");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i]=sc.nextInt();
+        }
+
+        System.out.println("Original Array :");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+ " ");
+        }
+
+
+
+        System.out.println();
+        System.out.println("Reversed Array :");
+        reverseElement(arr);
+
+
+        sc.close();
+    }
+
+}
