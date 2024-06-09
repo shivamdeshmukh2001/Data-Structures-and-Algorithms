c1102e16b162207bc2ae461feafd0d548bd0ce64
ShivamDeshmukh <sadeshmukh2001@gmail.com>
2024-06-09 23:10:26 +0530

    array
    Sorted array in one traversal

--- Arrays/TwoPointerSwapNumber.java
+++ Arrays/TwoPointerSwapNumber.java
@@ -11,27 +11,52 @@ public class TwoPointerSwapNumber {
         System.out.println();
     }
 
+    static void swap(int[] arr, int left, int right){
+        int temp = arr[left];
+        arr[left]=arr[right];
+        arr[right]= temp;
+    }
+
     
 
     static void sortZeroesAndOnes(int[] arr){
 
         int n =arr.length;
-        int zeroes =0;
+        // int zeroes =0;
+        int left =0;
+        int right = n-1;
 
         // count no of zeroes
-        for (int i = 0; i < n; i++) {
-            if(arr[i]==0){
-                zeroes++;
+        // for (int i = 0; i < n; i++) {
+        //     if(arr[i]==0){
+        //         zeroes++;
+        //     }
+        // }
+
+        // for (int i = 0; i < arr.length; i++) {
+        //     if (i < z) {
+        //         arr[i]=0;
+        //     } else {
+        //         arr[i]=1;
+        //     }
+        // }
+
+        while (left < right) {
+            if(arr[left] == 1 && arr[right] ==0){
+                swap(arr, left,right);
+                left++;
+                right--;
+            }
+
+            if (arr[left] ==0) {
+                left++;
+            }
+
+            if (arr[right] ==1) {
+                right--;
             }
         }
 
-        for (int i = 0; i < arr.length; i++) {
-            if (i < zeroes) {
-                arr[i]=0;
-            } else {
-                arr[i]=1;
-            }
-        }
     }
 
 
