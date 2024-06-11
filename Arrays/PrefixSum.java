8535ada71f1427c677ce678309c45f5d79450c08
ShivamDeshmukh <sadeshmukh2001@gmail.com>
2024-06-11 21:06:27 +0530

    array

---
+++ Arrays/PrefixSum.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class PrefixSum {
+
+    static void printArray(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print( arr[i]+" ");
+        }
+    }
+
+    static int[] prefixSum(int[] arr){
+        int n= arr.length;
+        // int[] pref = new int[n];
+        // pref[0] = arr[0];
+
+        for (int i = 1; i < n; i++) {
+            arr[i] = arr[i-1] + arr[i];
+        }
+        return arr;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size of Array :");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        System.out.println("Enter "+n+" elements :");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int[] pref = prefixSum(arr);
+        System.out.println("Prefix sum of Array :");
+        printArray(pref);
+
+        sc.close();
+    }
+}
